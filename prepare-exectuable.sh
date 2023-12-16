716fe8f9a4ddac5f98e25f85bb01096b35ff110b
PratyushKing <83279568+PratyushKing@users.noreply.github.com>
2023-12-16 13:27:09 +0530

    move to makefiles

--- prepare-exectuable.sh
+++ prepare-exectuable.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-rm -rf bin/
-rm cosmos
-dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true
-cp bin/Release/net7.0/linux-x64/publish/cosmos .
-echo "Executable sent to base source directory!"
+# rm -rf bin/
+# rm cosmos
+# dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true
+# cp bin/Release/net7.0/linux-x64/publish/cosmos .
+# echo "Executable sent to base source directory!"
+
+echo "Please use make exe instead of this command."
